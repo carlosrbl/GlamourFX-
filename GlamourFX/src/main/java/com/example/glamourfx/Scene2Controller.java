d98e53976567b9556552ae40ab64ef0cb38a6a6b
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2025-05-02 17:49:24 +0200

    New Proyect Updated

---
+++ GlamourFX/src/main/java/com/example/glamourfx/Scene2Controller.java
@@ -0,0 +1,7 @@
+package com.example.glamourfx;
+
+import javafx.fxml.FXML;
+
+public class Scene2Controller {
+    // Aquí puedes agregar la lógica para la segunda vista si es necesario
+}
---
+++ GlamourFX/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.glamourfx
+{
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.example.glamourfx to javafx.fxml;
+    exports com.example.glamourfx;
+}
