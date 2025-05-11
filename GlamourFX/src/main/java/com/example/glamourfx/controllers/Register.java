4f26cb6855f650d232c074f8257761cd6c26ecb0
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2025-05-11 21:04:37 +0200

    New interface Customer, it´s a probe

--- GlamourFX/src/main/java/com/example/glamourfx/controllers/Register.java
+++ GlamourFX/src/main/java/com/example/glamourfx/controllers/Register.java
@@ -1,5 +1,6 @@
 package com.example.glamourfx.controllers;
 
+import com.example.glamourfx.data.Customer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,9 +14,36 @@ import java.io.IOException;
 public class Register {
 
     @FXML
-    private void addUser()
+    private void addUser(ActionEvent event)
     {
+        try {
+            String clientName = newUser();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/glamourfx/customer.fxml"));
+            Parent root = loader.load();
+
+            CustomerInterface controller = loader.getController();
+            controller.setClienteName(clientName);
+
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setTitle("GlamourFX");
+            stage.setScene(scene);
+
+            stage.setFullScreen(true);
+            stage.setFullScreenExitHint("");
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String newUser()
+    {
+        Customer customer = new Customer("Sergio","a","a","a");
+        return customer.getName();
     }
 
     @FXML
